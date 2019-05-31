8f7cc0804bfe45cb501428ff3bf0a1beb43b5979
Ivan Hernandez <gamis214@gmail.com>
2019-05-31 11:33:52 -0500

    Update BroadCastPDF.java

--- app/src/main/java/com/example/s_hernandezivah/downloadmanager_example/BroadCastPDF.java
+++ app/src/main/java/com/example/s_hernandezivah/downloadmanager_example/BroadCastPDF.java
@@ -1,3 +1,18 @@
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.FileProvider;
+import java.io.File;
+
 /****** CLASE DE BORADCAST PARA USO CON VERSIONES MAYORES A API 27 -> N ******/
 public class BroadCastPDF extends BroadcastReceiver {
 
