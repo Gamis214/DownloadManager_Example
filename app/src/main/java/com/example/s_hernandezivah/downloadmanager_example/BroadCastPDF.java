fe69dbeaa724ec16c27ecaacb9d7ae393166be88
Ivan Hernandez <gamis214@gmail.com>
2019-05-31 11:32:17 -0500

    Create BroadCastPDF.java

---
+++ app/src/main/java/com/example/s_hernandezivah/downloadmanager_example/BroadCastPDF.java
@@ -0,0 +1,59 @@
+public class BroadCastPDF extends BroadcastReceiver {
+
+    private String fileName;
+    private String CHANNEL_ID = "CH_001";
+    private CharSequence name = "CH_BROADCAST";
+    private String Description = "D_BROADCAST";
+
+    /**
+     * Constructor
+     * @param downloadFilePath nombre del archivo de descarga
+     */
+    public BroadCastPDF(String fileName){
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //-->Generamos la ruta donde se ubica el archivo descargado
+        File filePath = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/",fileName);
+        //-->Creamos el intent dependiendo de la version para agregarlo a la notificacion y abrirlo al dar click
+        Intent i = new Intent();
+        i.setAction(android.content.Intent.ACTION_VIEW);
+        i.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Uri apkURI = FileProvider.getUriForFile(context,
+                context.getApplicationContext().getPackageName() + ".provider", filePath);
+            i.setDataAndType(apkURI, "application/pdf");
+        } else {
+            i.setDataAndType(Uri.fromFile(filePath), "application/pdf");
+        }
+        PendingIntent pIntent = PendingIntent.getActivity(context, 0, i, 0);
+
+        //--> Creamos la notificacion **************************************************************
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.app_icon_miclaro)
+                .setContentTitle("Factura Descargada")
+                .setContentText("Abrir Factura")
+                .setContentIntent(pIntent)
+                .setAutoCancel(true);
+
+        mNotificationManager.notify(1001,builder.build());
+    }
+
+}
